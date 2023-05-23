ccfa5cf9339263834cc793bd29bdf149c2dcc57b
Eylachap <eylachap@gmail.com>
2023-05-22 21:28:48 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function showAlert() { alert('¡Hola, es es un mensaje de alerta desde JavaScript!'); }
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
